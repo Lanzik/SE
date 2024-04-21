f378df04e4ff5a69cc4e6989d04369359b2911ba
lanzik <lanzikm@gmail.com>
2024-04-21 23:06:31 +0330

    delete extra files

--- src/main/java/ir/ramtung/tinyme/domain/entity/MatchingOutcome.java
+++
@@ -1,9 +0,0 @@
-package ir.ramtung.tinyme.domain.entity;
-
-public enum MatchingOutcome {
-    EXECUTED,
-    QUEUED_AS_INACTIVE_ORDER,
-    NOT_ENOUGH_CREDIT,
-    NOT_ENOUGH_POSITIONS,
-    NOT_ENOUGH_INITIAL_TRANSACTION
-}
